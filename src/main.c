af8eeac66398edcf241369f62949f9b80c8fe8d7
zhf521 <3407085928@qq.com>
2024-03-31 19:53:05 +0800

    更新修改密码功能

--- src/main.c
+++ src/main.c
@@ -4,11 +4,12 @@
 #include "OLED.h"
 #include "Delay.h"
 #include "string.h"
-
-#define password "123456"
+#include "stdio.h"
 
 int main(void)
 {
+    // 密码
+    char password[6] = "123456";
     // 初始化继电器、OLED
     Relay_Init();
     OLED_Init();
@@ -21,6 +22,10 @@ loop:
     char inputKeyValue = '\0';
     // 输入的密码
     char inputPassword[6] = {'\0'};
+    // 是否处于修改密码模式
+    int changePasswordMode = 0;
+    // 管理员密码
+    char adminPassword[6] = "000000";
     // 当前输入的密码位数
     int count = 0;
     while (1) {
@@ -44,31 +49,72 @@ loop:
         if (inputKeyValue == '*') {
             Delay_ms(100);
             if (MatrixKey_GetValue() == inputKeyValue) {
-                if (strcmp(inputPassword, password) == 0) {
+                if (changePasswordMode == 1) {
+                    // 判断是否处于修改密码模式(确认输入管理员密码)
+                    if (strcmp(inputPassword, adminPassword) == 0) {
+                        OLED_Clear();
+                        OLED_ShowChinese(20, 0, "请输入新密码：");
+                        OLED_ShowString(20, 24, "_ _ _ _ _ _", OLED_8X16);
+                        OLED_Update();
+                        changePasswordMode = 2;
+                    } else {
+                        OLED_Clear();
+                        OLED_ShowChinese(10, 0, "管理员密码错误");
+                        OLED_Update();
+                        Delay_s(2);
+                        OLED_Clear();
+                        goto loop;
+                    }
+                } else if (changePasswordMode == 2) {
+                    for (int i = 0; i < 6; i++) {
+                        password[i] = inputPassword[i];
+                    }
+                    printf("密码已修改为：%s\n", password);
                     OLED_Clear();
-                    OLED_ShowChinese(0, 0, "密码正确，已开锁");
+                    OLED_ShowChinese(30, 0, "修改成功");
                     OLED_Update();
-                    Relay_On();
                     Delay_s(2);
                     OLED_Clear();
-                    Relay_Off();
                     goto loop;
                 } else {
-                    OLED_Clear();
-                    OLED_ShowChinese(0, 0, "密码错误，请重试");
-                    OLED_Update();
-                    Delay_s(2);
-                    OLED_Clear();
-                    goto loop;
+                    if (strcmp(password, inputPassword) == 0) {
+                        OLED_Clear();
+                        OLED_ShowChinese(0, 0, "密码正确，已开锁");
+                        OLED_Update();
+                        Relay_On();
+                        Delay_s(2);
+                        OLED_Clear();
+                        Relay_Off();
+                        goto loop;
+                    } else {
+                        OLED_Clear();
+                        OLED_ShowChinese(0, 0, "密码错误，请重试");
+                        OLED_Update();
+                        Delay_s(2);
+                        OLED_Clear();
+                        goto loop;
+                    }
                 }
             }
         }
-        //    清除输入密码
+        // 清除输入密码
         if (inputKeyValue == '#') {
             Delay_ms(100);
             if (MatrixKey_GetValue() == inputKeyValue) {
                 memset(inputPassword, '\0', sizeof(inputPassword));
                 count = 0;
+                OLED_ShowString(20, 24, "_ _ _ _ _ _", OLED_8X16);
+                OLED_Update();
+            }
+        }
+        // 修改密码
+        if (inputKeyValue == 'A') {
+            Delay_ms(100);
+            if (MatrixKey_GetValue() == inputKeyValue) {
+                changePasswordMode = 1;
+                count              = 0;
+                OLED_Clear();
+                OLED_ShowChinese(0, 0, "请输入管理员密码");
                 OLED_ShowString(20, 24, "_ _ _ _ _ _", OLED_8X16);
                 OLED_Update();
             }
